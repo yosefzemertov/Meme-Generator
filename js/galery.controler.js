b3738626b6438022dce279f0d2df3266a91af549
yosefzemertov <yosizemertov@gmail.com>
2022-02-19 21:35:52 +0200

    gallery search

--- js/galery.controler.js
+++ js/galery.controler.js
@@ -1,20 +1,42 @@
 'use strict'
 
 function init() {
-renderGalery()
+    renderStraccher()
+    renderGalery()
+}
+
+function renderStraccher() {
+    var strHTML = '<input type="text" name="filter" oninput="filterGallery(this)"> <div class="grid-container"></div>'
+    document.querySelector('.main').innerHTML = strHTML
 }
 
 function renderGalery() {
-    var strHTML = '<div class="grid-container">'
     var imgs = getimgs()
+    var strHTML;
     imgs.forEach(img => strHTML += `<img onClick="onImgSelect(this)" class="${img.id}" src="${img.url}">`)
-    strHTML += '</div>'
-    document.querySelector('.main').innerHTML = strHTML
+    document.querySelector('.grid-container').innerHTML = strHTML;
+
+
+}
+
+function renderFilterGalery(word) {
+    var imgs = getimgs()
+    var strHTML;
+    var image = imgs.filter(img => {
+        if (img.keyWord.join('').includes(word)) return img
+    })
+    image.forEach(img => strHTML += `<img onClick="onImgSelect(this)" class="${img.id}" src="${img.url}">`)
+    document.querySelector('.grid-container').innerHTML = strHTML;
+console.log(image);
 }
 
 function onImgSelect(el) {
     setChoosimg(+el.classList.value)
     renderCan();
+}
+
+function filterGallery(elFilter) {
+    renderFilterGalery(elFilter.value);
 }
 
 
